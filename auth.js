a57cca33e86656ab86ae41084c8886dcfcef7e91
Hardik0101 <hardik.prajapati@seaflux.tech>
2024-02-06 12:50:17 +0530

    login and signup added

--- auth.js
+++ auth.js
@@ -2,13 +2,23 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyDuhtZl4f3lLnwfd3WZ5qgEnfl0yYt75uA";
 
+export async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+  console.log("====================================");
+  console.log(response.data);
+  console.log("====================================");
+}
+
 export async function createUser(email, password) {
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key= " + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
